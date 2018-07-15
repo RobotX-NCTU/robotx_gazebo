df7ce23ec5fadb9387888b5443033cdfb73a9fc9
Tony Hsiao <tonycar12002@gmail.com>
2018-07-15 20:27:21 +0800

    update shell script

--- dependencies_common.sh
+++ dependencies_common.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-set -e
-
-if [[ `id -u` -eq 0 ]] ; then
-    echo "Do not run this with sudo (do not run random things with sudo!)." ;
-    exit 1 ;
-fi
-
-set -x
-
-
 sudo apt install -y \
 	ros-kinetic-twist-mux-* \
 	ros-kinetic-geographic-* \
@@ -31,7 +21,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install mercurial cmake pkg-config python python-pip git gazebo7 libgazebo7-dev ros-kinetic-ros-base ros-kinetic-teleop-tools ros-kinetic-teleop-twist-keyboard ros-kinetic-gazebo-plugins ros-kinetic-gazebo-ros ros-kinetic-xacro ros-kinetic-joy libeigen3-dev wget
-pip install --upgrade pip
-pip install packaging
-pip install vcstools
-sudo pip install appdirs
+pip2 install --upgrade pip
+pip2 install packaging
+pip2 install vcstools
+sudo pip2 install appdirs

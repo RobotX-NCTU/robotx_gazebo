330e364297acbe6811371c55cd847b065b19e0e4
Tony Hsiao <tonycar12002@gmail.com>
2018-09-19 20:00:39 +0800

    update depend

--- dependencies_common.sh
+++ dependencies_common.sh
@@ -16,7 +16,8 @@ sudo apt install -y \
         ros-kinetic-geometry2 \
         ros-kinetic-velodyne-* \
         libignition-math2-dev \
-	ros-kinetic-hector-gazebo-plugins
+	ros-kinetic-hector-gazebo-plugins \
+	libignition-math2-dev 
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
